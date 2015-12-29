4222e5240dc13fd46f80d4ccfe0e348ee09d2a4f
Kostas Krevatas <297mxq@gmail.com>
2015-12-29 05:18:32 -0500

    initial commit

---
+++ modules/gateways/callback/everypay.php
@@ -0,0 +1,116 @@
+<?php
+# Required File Includes
+include("../../../dbconnect.php");
+include("../../../includes/functions.php");
+include("../../../includes/gatewayfunctions.php");
+include("../../../includes/invoicefunctions.php");
+
+$gatewaymodule = "everypay";
+
+$params = getGatewayVariables($gatewaymodule);
+
+# Checks gateway module is active before accepting callback
+if (!$params["type"]){
+    die("Module Not Activated");
+}
+
+if (!isset($_POST["everypayToken"])){
+    header("Location: " . $params['systemurl']);
+}   
+
+# Get Returned Variables
+$merchant_order_id = $_POST["merchant_order_id"];
+$everypay_token = $_POST["everypayToken"];
+
+# Checks invoice ID is a valid invoice number or ends processing
+$merchant_order_id = checkCbInvoiceID($merchant_order_id, $params["name"]);
+
+# Checks transaction number isn't already in the database and ends processing if it does
+checkCbTransID($everypay_token);
+
+# Fetch invoice to get the amount
+$result = mysql_fetch_assoc(select_query('tblinvoices', 'total', array("id" => $merchant_order_id)));
+$amount = $result['total'];
+
+# Check if amount is EURO, convert if not.
+$currency = getCurrency();
+if ($currency['code'] !== 'EUR') {
+    $result = mysql_fetch_array(select_query("tblcurrencies", "id", array("code" => 'EUR')));
+    $inr_id = $result['id'];
+    $converted_amount = convertCurrency($amount, $currency['id'], $inr_id);
+} else {
+    $converted_amount = $amount;
+}
+
+$converted_amount = 100 * $converted_amount;
+
+$success = true;
+$error = "";
+
+try {
+
+    $theURL = "https://" . ($params['mode'] == 'LIVE' ? '' : 'sandbox-') 
+             . "api.everypay.gr/payments";
+    $everypayParams = array(
+        'token' => $everypay_token,
+        'amount' => $converted_amount,
+        'description' => $CONFIG['CompanyName'] . " Invoice #" . $merchant_order_id,
+        'payee_email' => $_POST["payee_email"],
+        'payee_phone' => $_POST["payee_phone"],
+    );
+
+    $curl = curl_init();
+    $query = http_build_query($everypayParams, null, '&');
+
+    curl_setopt($curl, CURLOPT_TIMEOUT, 60);
+    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST');
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+    curl_setopt($curl, CURLOPT_USERPWD, $params['SecretKey'] . ':');
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $query);
+    curl_setopt($curl, CURLOPT_URL, $theURL);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+    $result = curl_exec($curl);
+    
+    $info = curl_getinfo($curl);
+
+    if ($result === false) {
+        $success = false;
+        $error = 'Curl error: ' . curl_error($curl);
+    } else {
+        $response_array = json_decode($result, true);
+        //Check success response
+        if (isset($response_array['error']) === false) {
+            $success = true;
+        } else {
+            $success = false;
+
+            if (!empty($response_array['error']['code'])) {
+                $error = $response_array['error']['code'] . ":" . $response_array['error']['message'];
+            } else {
+                $error = "EVERYPAY_ERROR:Invalid Response <br/>" . $result;
+            }
+        }
+    }
+    //close connection
+    curl_close($curl);
+} catch (Exception $e) {
+    $success = false;
+    $error = "WHMCS_ERROR:Request to Everypay Failed";
+}
+
+if ($success === true) {
+    # Successful 
+    # Apply Payment to Invoice: invoiceid, transactionid, amount paid, fees, modulename
+    addInvoicePayment($merchant_order_id, $everypay_payment_id, $amount, 0, $params["name"]);
+    logTransaction($params["name"], json_encode($_POST), "Successful: " . $result); # Save to Gateway Log: name, data array, status
+} else {
+    # Unsuccessful
+    # Save to Gateway Log: name, data array, status
+    logTransaction($params["name"], json_encode($_POST), "Unsuccessful: Error" . $error . ". Please check everypay dashboard for Payment token: " . $everypay_token);
+}
+
+header("Location: " . $params['systemurl'] . "/viewinvoice.php?id=" . $merchant_order_id);
+
+?>
---
+++ modules/gateways/everypay.php
@@ -0,0 +1,104 @@
+<?php
+
+function everypay_config()
+{
+
+    $configarray = array(
+        "FriendlyName" => array("Type" => "System", "Value" => "Everypay (Visa/Mastercard/Maestro)"),
+        "PublicKey" => array("FriendlyName" => "Public Key", "Type" => "text", "Size" => "50", "Description" => "Enter your Everypay Public Key here",),
+        "SecretKey" => array("FriendlyName" => "Secret Key", "Type" => "text", "Size" => "50", "Description" => "Enter your Everypay Secret Key here",),
+        "mode" => array("FriendlyName" => "Mode", "Type" => "dropdown", 'Options' => 'LIVE,SANDBOX', "Description" => "Choose the environment (live or sandbox). Note that in sandbox mode no charges will be applied to any card",),
+    );
+    return $configarray;
+}
+
+function everypay_link($params)
+{
+    # Gateway Specific Variables
+    $public_key = $params['PublicKey'];
+
+    # Invoice Variables
+    $order_id = $params['invoiceid'];
+    $description = $params["description"];  
+    
+    $amount = $params['amount'];    
+    
+    $currency = getCurrency();
+    # Check if amount is EURO, convert if not.     
+    if ($currency['code'] !== 'EUR') {
+        $result = mysql_fetch_array(select_query("tblcurrencies", "id", array("code" => 'EUR')));
+        $inr_id = $result['id'];
+        $converted_amount = convertCurrency($amount, $currency['id'], $inr_id);
+    } else {
+        $converted_amount = $amount;
+    }
+
+    $converted_amount = 100 * $converted_amount;
+
+
+    # System Variables
+    $callbackURL = $params['systemurl'] . '/modules/gateways/callback/everypay.php';
+
+
+    $html = '<form action="' . $callbackURL . '" method="POST" id="everypay-payment-form">
+        <div class="button-holder" style="margin-top:10px"></div>
+        <input type="hidden" name="merchant_order_id" id="order_id" value="' . $order_id . '"/>
+        <input type="hidden" name="payee_email" value="' . $params['clientdetails']['email'] . '"/>
+        <input type="hidden" name="payee_phone" value="' . $params['clientdetails']['phonenumber'] . '"/>
+    </form>
+    <style type="text/css">
+    .everypay-button{
+    
+    }
+    </style>';
+
+    $js = '<script src="https://button.everypay.gr/js/button.js"></script>';
+
+    $js .= '<script type="text/javascript">
+//<![CDATA[        
+    var EVERYPAY_OPC_BUTTON = {
+        amount: ' . $converted_amount . ',
+        description: "' . $description . '",
+        key: "' . $public_key . '",
+        locale: "en",
+        sandbox: ' . ($params['mode'] == 'LIVE' ? 0 : 1) . '
+    }    
+    
+    var autoPressButton = true;    
+    try {
+        //case there is jquery
+        if ($(\'select[name="gateway"]\').length){
+            autoPressButton = false;
+        }
+        
+        $("#everypay-payment-form").unbind(\'submit\').bind(\'submit\', function(e){
+            e.preventDefault();
+            if ($(this).find(\'everypayToken\').length){
+                $(this).unbind(\'submit\').submit();
+            }
+        });        
+    } catch(err) {        
+        //probably no jquery - it is the standalone invoice  and do not autopress
+        autoPressButton = false;
+    }
+    
+    var loadButton = setInterval(function () { 
+        try {                  
+            var $everypayForm = document.getElementById("everypay-payment-form"); 
+            EverypayButton.jsonInit(EVERYPAY_OPC_BUTTON, $everypayForm);
+            if (autoPressButton){
+                setTimeout(function(){ 
+                    var x = document.getElementsByClassName("everypay-button");
+                    x[0].click();
+                }, 300);                                
+            }
+            clearInterval(loadButton);
+        } catch (err){}
+    }, 100);
+    
+//]]>
+</script>';
+    return $html . $js;
+}
+
+?>
